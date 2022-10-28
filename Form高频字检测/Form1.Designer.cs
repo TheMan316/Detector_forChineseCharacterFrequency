a4a86b8a3e00704f42d6f039a45160719d4e5ccd
runfeng <2412395774@qq.com>
2022-10-28 19:19:31 +0800

    添加项目文件。

---
+++ Form高频字检测/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Form高频字检测 {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gB18030ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uTF8ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem,
+            this.设置ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(436, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.打开ToolStripMenuItem.Text = "打开文件";
+            this.打开ToolStripMenuItem.Click += new System.EventHandler(this.打开ToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "保存为新文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(154, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "整合进原字频文件";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(186, 57);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "开始检测";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.设置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gB18030ToolStripMenuItem,
+            this.uTF8ToolStripMenuItem});
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.设置ToolStripMenuItem.Text = "选择编码";
+            this.设置ToolStripMenuItem.Click += new System.EventHandler(this.设置ToolStripMenuItem_Click);
+            // 
+            // gB18030ToolStripMenuItem
+            // 
+            this.gB18030ToolStripMenuItem.Name = "gB18030ToolStripMenuItem";
+            this.gB18030ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.gB18030ToolStripMenuItem.Text = "GB18030";
+            this.gB18030ToolStripMenuItem.Click += new System.EventHandler(this.gB18030ToolStripMenuItem_Click);
+            // 
+            // uTF8ToolStripMenuItem
+            // 
+            this.uTF8ToolStripMenuItem.Name = "uTF8ToolStripMenuItem";
+            this.uTF8ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.uTF8ToolStripMenuItem.Text = "UTF-8";
+            this.uTF8ToolStripMenuItem.Click += new System.EventHandler(this.uTF8ToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(276, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "当前编码：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(364, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 284);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "汉字字频检测器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gB18030ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uTF8ToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form高频字检测/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form高频字检测 {
+    public partial class Form1 : Form {
+        string _fileName;
+        Dictionary<char, int> _dict = new Dictionary<char, int>();
+        List<char> _listWord = new List<char>();
+        List<int> _ListWordTimes = new List<int>();
+        string _encoding = "GB18030";
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e) {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                int fileLen = openFileDialog.FileName.LastIndexOf(@"/");
+                _fileName = openFileDialog.FileName.Substring(fileLen + 1);
+            }
+        }
+        private bool ContainSymbol(char word) {
+            if ((int)word <= 255) {
+                return true;
+            }
+            switch (word) {
+                case '。':
+                    return true;
+                case '，':
+                    return true;
+                case ' ':
+                    return true;
+                case '：':
+                    return true;
+                case '”':
+                    return true;
+                case '“':
+                    return true;
+                case '·':
+                    return true;
+                case '●':
+                    return true;
+                case '│':
+                    return true;
+                case '┌':
+                    return true;
+                case '─':
+                    return true;
+                case '┐':
+                    return true;
+                case '└':
+                    return true;
+                case '┘':
+                    return true;
+                case '；':
+                    return true;
+                case '、':
+                    return true;
+                case '？':
+                    return true;
+                case '！':
+                    return true;
+                case '\r':
+                    return true;
+                case '\n':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            string Chinese = File.ReadAllText(_fileName, Encoding.GetEncoding(_encoding));
+            int a = 0;
+            foreach (var word in Chinese) {
+                if (ContainSymbol(word) == false) {
+                    if (_dict.ContainsKey(word) == false) {
+                       
+                        _dict.Add(word, 1);
+                    }
+                    else {
+                      
+                        _dict[word]++;
+                    }
+                }
+            }
+            foreach (var keyAndValue in _dict) {
+                if (keyAndValue.Key == '史') {
+                    a++;
+                }
+                _listWord.Add(keyAndValue.Key);
+                _ListWordTimes.Add(keyAndValue.Value);
+            }
+            QuickSort(0, _listWord.Count - 1);
+            MessageBox.Show("检测完成！");
+        }
+        private void QuickSort(int leftIndex, int rightIndex) {
+            if (leftIndex >= rightIndex) {
+                return;
+            }
+            int baseValue = _ListWordTimes[leftIndex];
+            char baseWord = _listWord[leftIndex];
+            int i = leftIndex;
+            int j = rightIndex;
+            int temp = 0;
+            char tempWord = ' ';
+            while (i < j) {
+                while (_ListWordTimes[j] <= baseValue && i < j) j--;
+                while (_ListWordTimes[i] >= baseValue && i < j) i++;
+                if (i < j) {
+                    temp = _ListWordTimes[i];
+                    tempWord = _listWord[i];
+                    _ListWordTimes[i] = _ListWordTimes[j];
+                    _listWord[i] = _listWord[j];
+                    _ListWordTimes[j] = temp;
+                    _listWord[j] = tempWord;
+                }
+                else {
+                    _listWord[leftIndex] = _listWord[i];
+                    _listWord[i] = baseWord;
+                    _ListWordTimes[leftIndex] = _ListWordTimes[i];
+                    _ListWordTimes[i] = baseValue;
+                    
+
+                }
+
+            }
+            QuickSort(leftIndex,i-1);
+            QuickSort(i+1,rightIndex);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+           
+            int len = _listWord.Count * 3;
+            StringBuilder sb = new StringBuilder(len);
+
+            int index = 0;
+            for (int i = 0; i < _listWord.Count; i++) {
+                sb.Append(_listWord[index]);
+                sb.Append(_ListWordTimes[index].ToString());
+                sb.Append("\n");
+                index++;
+            }
+            File.WriteAllText("tempFile.txt", sb.ToString());
+            MessageBox.Show("输出完成！");
+
+        }
+
+        private void 设置ToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        private void gB18030ToolStripMenuItem_Click(object sender, EventArgs e) {
+            _encoding = "GB18030";
+            label2.Text = _encoding;
+
+        }
+
+        private void uTF8ToolStripMenuItem_Click(object sender, EventArgs e) {
+            _encoding = "UTF-8";
+            label2.Text = _encoding;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            label2.Text = _encoding;
+        }
+    }
+}
